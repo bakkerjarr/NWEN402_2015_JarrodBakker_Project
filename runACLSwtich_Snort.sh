b652d35d0716b6c0df413f3c802a09e1ee9ce100
Jarrod Bakker <jarrodbakker@hotmail.com>
2015-10-26 23:35:08 +1300

    Integrated ACLSwitch - flows matched in table 1 are mirrored to host 2 so that they may be scanned by Snort.

---
+++ runACLSwtich_Snort.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the stateful version of ACLSwitch with Snort integration
+cd /home/ubuntu/ryu && ./bin/ryu-manager --verbose ryu/NWEN402_2015_JarrodBakker_Project/SDIPS/ACLSwitch_Snort/acl_switch_stateful_snort.py
